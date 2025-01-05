8d921cbccdf75038da7abc173c41d8498d7f5ad8
Andrea Garau <andreagarau98@gmail.com>
2025-01-05 11:32:11 +0100

    Initial Commit

---
+++ WatchHapticList Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchHapticList Watch App
+//
+//  Created by Andrea Garau on 05/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchHapticList Watch App/WatchHapticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchHapticListApp.swift
+//  WatchHapticList Watch App
+//
+//  Created by Andrea Garau on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchHapticList_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
